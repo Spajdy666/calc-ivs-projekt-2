bdc638a74782601ab3159f56c5d939470c7fc734
Petr Čajánek <cajanek.petr.01@gmail.com>
2022-04-27 16:09:36 +0200

    Přidání hrubé verze knihovny

---
+++ ivs_projekt_2/src/mathlib.py
@@ -0,0 +1,29 @@
+class MathLibrary:
+
+    def add(a,b):
+        return a+b
+
+    def subtract(a,b):
+        return a-b
+
+    def divide(a,b):
+        #add division of 0 exception
+        return a/b
+
+    def multiply(a,b):
+        return a*b
+
+    def fact(a):
+        #fix negative values and decimal
+        if a == 1:
+            return a
+        elif a==0:
+            return 1
+        else:
+            return a*MathLibrary.fact(a-1)
+
+    def nthRoot(a,b):
+        #add negative values exception
+        return a**(1/b)
+
+#print(MathLibrary.fact(4))
