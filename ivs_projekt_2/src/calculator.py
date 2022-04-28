2bf3290a4f76c1afec73e3a801a0d6d510f1603d
Petr Čajánek <cajanek.petr.01@gmail.com>
2022-04-28 23:25:14 +0200

    Adding calculator functionality

--- ivs_projekt_2/src/calculator.py
+++ ivs_projekt_2/src/calculator.py
@@ -20,6 +20,13 @@ class CalculatorWindow(QtWidgets.QMainWindow,Ui_mainWindow):
         self.buttonMul.clicked.connect(self.mul)
         self.buttonDiv.clicked.connect(self.div)
         
+        self.buttonPow.clicked.connect(self.pow)
+        self.buttonRoot.clicked.connect(self.root)
+        self.buttonFact.clicked.connect(self.fact)
+        self.buttonLn.clicked.connect(self.ln)
+        
+        self.buttonEquals.clicked.connect(self.equals)
+                
     def add(self):
         a = self.lineEditInput1.text()
         a=self.returnFloat(a)
@@ -62,6 +69,37 @@ class CalculatorWindow(QtWidgets.QMainWindow,Ui_mainWindow):
             self.lineEditResult.setText(str(MathLibrary.divide(a, b)))
         except:
             self.showErrorDialog("Division with 0 is illegal.")
+            
+    def pow(self):
+        self.lineEditSign.setText('^')
+        return
+    
+    def fact(self):
+        self.lineEditSign.setText('!')
+        return
+        
+    def root(self):
+        self.lineEditSign.setText('√')
+        return
+        
+    def ln(self):
+        self.lineEditSign.setText("ln")
+        return
+    
+    def equals(self):
+        sign = self.lineEditSign.text()
+        a = self.lineEditInput1.text()
+        a=self.returnFloat(a)
+        b = self.lineEditInput2.text()
+        b=self.returnFloat(b)
+        try:
+            if sign=='^':
+                self.lineEditResult.setText(str(MathLibrary.power(a, b)))
+            elif sign=='√':
+                self.lineEditResult.setText(str(MathLibrary.root(a, b)))
+        except ValueError as e:
+            self.showErrorDialog(str(e))
+    
         
     def returnFloat(self,a):
         number = a
@@ -80,4 +118,4 @@ if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
     window = CalculatorWindow()
     window.show()
-    sys.exit(app.exec_())         
+    sys.exit(app.exec_())     
