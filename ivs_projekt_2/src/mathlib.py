662e47b99e8db441cf3958f26e057ce7300075a7
Petr Čajánek <cajanek.petr.01@gmail.com>
2022-04-28 21:25:32 +0200

    Added more error handling

--- ivs_projekt_2/src/mathlib.py
+++ ivs_projekt_2/src/mathlib.py
@@ -10,7 +10,7 @@ class MathLibrary:
     def divide(a,b):
         #add division of 0 exception
         if b==0:
-            raise Exception("Can not divide with zero.")
+            raise ValueError("Can not divide with zero.")
         return a/b
 
     def multiply(a,b):
@@ -18,21 +18,44 @@ class MathLibrary:
 
     def fact(a):
         #fix negative values and decimal
-        if a == 1:
-            return a
-        elif a==0:
+        try:
+            convertToInt(a)
+        except:
+            raise ValueError("Factorial must be natural number")
+        if a<0:
+            raise ValueError("Factorial value must be greater than 0.")
+        if a==0:
             return 1
-        else:
-            return a*MathLibrary.fact(a-1)
-
+        fact=1
+        for i in range(1,a+1):
+            fact*=i
+        
+        return fact
+            
     def root(a,b):
         #add negative values exception
+        if b<0:
+            raise ValueError("Exponent must be greater than 0.")
+        if a<0 and b%2!=0:
+            raise ValueError("%dth root must have a positive value",b)
         return a**(1/b)
 
     def power(a,b):
+        try:
+            convertToInt(b)
+        except:
+            raise ValueError("Exponent must be natural number")
+        if b<0:
+            raise ValueError("Exponent must be greater than 0.")
         return a**b
     
     def ln(a):
+        if a<0:
+            raise ValueError("Number must be higher than 0")
+            return
         return math.log(a)
+    
+    def convertToInt(a):
+        return int(a)
 
-#print(MathLibrary.ln(5))
+#print(MathLibrary.fact(3))
